da07828837254fc20fea0962647e87803784b5c9
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-09-13 14:53:57 -0400

    implemented fancybox for image handling

--- personalCode/JS/freeCodeCampCourse/test.js
+++ personalCode/JS/freeCodeCampCourse/test.js
@@ -1,12 +1,16 @@
-function rangeOfNumbers(startNum, endNum) {
-  if (endNum - startNum === 0) {
-    return [startNum];
-  } else {
-    var myArray = rangeOfNumbers(startNum, endNum - 1);
-    myArray.push(endNum);
-    return myArray;
-  }
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
 };
 
-var i = rangeOfNumbers(1, 10);
-console.log(i);
+// Only change code below this line
+
+const {
+  today: {
+    low: lowToday,
+    high: highToday
+  }
+} = LOCAL_FORECAST;
+
+// Only change code above this line
