a6cb8f8638771f27a9a4e8b7847ae7a7f81d0576
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-12-12 16:43:47 -0500

    brushing up on java for interview

--- Java/JavaLaptopSetup/src/com/company/Main.java
+++ Java/JavaLaptopSetup/src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Hello World");
         System.out.print("\nTime to brush up on Java");
+        myClass myClass1 = new myClass();
+        System.out.print(myClass1.x);
     }
 }
---
+++ Java/JavaLaptopSetup/src/com/company/myClass.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class myClass {
+
+
+    int x = 1;
+
+    public static void printHi(String[] args) {
+        System.out.print("hi");
+    }
+
+}
