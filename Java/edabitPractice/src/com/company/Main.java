ced10043591745432522aa10474c5ed77bf2a276
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-12-19 19:29:24 -0500

    java practice

--- Java/edabitPractice/src/com/company/Main.java
+++ Java/edabitPractice/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        boolean test = test1(1, 1);
+        int test = test5("what on earth is even going on here");
         System.out.println(test);
     }
 
@@ -21,10 +21,30 @@ public class Main {
 
     //test2 done!
     public static int test2(int base, int height) {
-        int answer = 0;
-        answer = (base * height) / 2;
+        int answer = (base * height) / 2;
         return answer;
     }
 
+    //test3 done!
+    public static int test3(int hours) {
+        int answer = hours * 3600;
+        return answer;
+    }
 
+    //test4 done!
+    public static int test4(int num) {
+        int answer = num + 1;
+        return answer;
+    }
+
+    //test5 done!
+    public static int test5(String s) {
+        int answer = 1;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                answer++;
+            }
+        }
+        return answer;
+    }
 }
