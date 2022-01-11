27bad582439d59c796417275ad438ed3ca1c7a52
njclark1 <77775946+njclark1@users.noreply.github.com>
2022-01-11 00:39:12 -0500

    Learning Java Frames

---
+++ Java/JFramePractice/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyFrame myFrame = new MyFrame();
+
+    }
+
+}
