f4d94c3fd01db0de767e76eff683dd4e0925a722
njclark1 <77775946+njclark1@users.noreply.github.com>
2022-02-01 23:02:38 -0500

    spring

--- Java/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Java/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	//main
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
