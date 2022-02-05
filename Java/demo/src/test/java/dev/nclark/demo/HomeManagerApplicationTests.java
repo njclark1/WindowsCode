1fe0b494f4f3f426e6839ef267190a2da9871b12
njclark1 <77775946+njclark1@users.noreply.github.com>
2022-02-05 00:16:21 -0500

    setting up mac environment for java

---
+++ Java/demo/src/test/java/dev/nclark/demo/HomeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nclark.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
