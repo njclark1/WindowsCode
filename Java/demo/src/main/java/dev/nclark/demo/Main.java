3c64193e90f6f30f7e31cc53b2d03e052a71b586
njclark1 <77775946+njclark1@users.noreply.github.com>
2022-02-05 02:22:42 -0500

    nothing works, we'll get em next time

--- Java/demo/src/main/java/dev/nclark/demo/Main.java
+++ Java/demo/src/main/java/dev/nclark/demo/Main.java
@@ -1,7 +1,10 @@
 package dev.nclark.demo;
 
+import static java.lang.System.out;
+import org.apache.catalina.startup.HomesUserDatabase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class Main {
--- Java/demo/src/main/java/dev/nclark/demo/Person.java
+++ Java/demo/src/main/java/dev/nclark/demo/Person.java
@@ -1,4 +1,16 @@
 package dev.nclark.demo;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+
+
 public class Person {
+    int id;
+    String firstName;
+    String lastName;
+
+    public static void printHi () {
+        System.out.println("Hi");
+    }
+
 }
