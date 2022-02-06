494e973719802966586e21bd5fe804edeac69b9b
njclark1 <77775946+njclark1@users.noreply.github.com>
2022-02-06 12:25:42 -0500

    replicating the project I made in C#, but in Java

---
+++ Java/HomeManager/src/test/java/dev/nclark/HomeManager/HomeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nclark.HomeManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
