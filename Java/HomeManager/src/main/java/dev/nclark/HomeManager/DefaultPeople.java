0614c683b670d1b341659dd0adfdf62a8702de7d
njclark1 <77775946+njclark1@users.noreply.github.com>
2022-02-07 17:24:52 -0500

    Update DefaultPeople.java

--- Java/HomeManager/src/main/java/dev/nclark/HomeManager/DefaultPeople.java
+++ Java/HomeManager/src/main/java/dev/nclark/HomeManager/DefaultPeople.java
@@ -4,8 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import java.util.Scanner;
-
 @Component
 public class DefaultPeople implements CommandLineRunner {
 
